99c37a9bf24ff9e5c19ef985cf61e9d0afe34565
SSH key <46862135+arthameez@users.noreply.github.com>
2022-06-26 16:22:53 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,20 @@
-from datetime import datetime
-from flask import Flask, jsonify, request
-from elasticsearch import Elasticsearch
+FROM python:3.7.7-slim
 
-# from api.elastic_test import connect_elasticsearch
-# from flask_app import app
-# from config.config_handling import get_config_value
+ENV PYTHONUNBUFFERED=1
 
+COPY * /opt/microservices/
+COPY requirements.txt /opt/microservices/
+RUN pip install --upgrade pip \
+  && pip install --upgrade pipenv\
+  && apt-get clean \
+  && apt-get update \
+  && apt install -y build-essential \
+  && apt install -y libmariadb3 libmariadb-dev \
+  && pip install --upgrade -r /opt/microservices/requirements.txt
 
-es = Elasticsearch('https://localhost:9200')
+USER 1001
 
-app = Flask(__name__)
+EXPOSE 8080
+WORKDIR /opt/microservices/
 
-
-##Health endpoint
-@app.route('/health', methods=['POST'])
-def health_check():
-    return "OK", 200
-
-
-@app.route('/insert_data', methods=['POST'])
-def insert_data():
-    if request.form['operation'] == 'insert':
-        city_id = request.form['city_id']
-        city = request.form['city']
-        population = request.form['population']
-        city_obj = {
-            'city_id': city_id,
-            'city': city,
-            'occupation': population
-        }
-
-        result = es.index(index='city', id=city_id, body=city_obj, request_timeout=30)
-        return jsonify(result)
-    elif request.form['operation'] == 'update':
-        city_id = request.form['city_id']
-        param = request.form['param']
-        new_val = request.form['new_value']
-
-        update_dict = {
-            'doc': {
-                param: new_val
-            }
-        }
-
-        response = es.update(index='city', id=city_id, body=update_dict)
-        return jsonify(response)
-    else:
-        return "Internal Server Error", 500
-
-
-@app.route('/get-population', methods=['GET'])
-def search_user():
-    city = request.form['city']
-
-    query_body = {
-        "query": {
-            "match": {
-                "name": city
-            }
-        }
-    }
-
-    res = es.search(index="city", body=query_body)
-
-    return jsonify(res['city']['population'])
+CMD ["python", "app.py", "8080"]
